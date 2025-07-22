b24f3f2ce06262b5641b426def5d5ded622d0b40
Sanjay Gupta <71681467+Sanjdcool@users.noreply.github.com>
2025-07-22 16:56:34 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,8 +14,11 @@ dotenv.config();
 // app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
 app.use(express.json({ limit: '10mb' }))
-app.use(cors())
-
+// app.use(cors())
+app.use(cors({
+    origin: 'https://school-management-systemsan.netlify.app',
+    credentials: true
+  }))
 mongoose
     .connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
